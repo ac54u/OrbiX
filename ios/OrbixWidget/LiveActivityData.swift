e208bb837b627a89bee2e9217129165dba97ed43
ac54u <thkceo@163.com>
2026-03-21 20:25:28 +0800

    测试

---
+++ ios/OrbixWidget/LiveActivityData.swift
@@ -0,0 +1,8 @@
+//
+//  LiveActivityData.swift
+//  Runner
+//
+//  Created by mac on 2026/3/21.
+//
+
+import Foundation
---
+++ ios/OrbixWidget/OrbixWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  OrbixWidgetBundle.swift
+//  OrbixWidget
+//
+//  Created by mac on 2026/3/21.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct OrbixWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        OrbixWidget()
+        OrbixWidgetLiveActivity()
+    }
+}
